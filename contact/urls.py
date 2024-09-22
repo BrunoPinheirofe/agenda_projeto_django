8b351d7ca4136681512391254df27932d22225c1
Bruno Pinheiro <bruno2013pinheiro@gmail.com>
2024-09-22 15:29:10 -0300

    adicionando pagina de 'contanto nao encontrado'

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,7 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'), 
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,39 +1,42 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 
 from contact.models import Contact
 
 
+
 def index(request):
-    contacts = Contact.objects \
-        .filter(show=True)\
-        .order_by('-id')[10:20]
+    contacts = Contact.objects.filter(show=True).order_by("-id")[10:20]
 
-    context = {
-        'contacts': contacts,
-        'site_title': 'Contatos - '
-    }
+    context = {"contacts": contacts, "site_title": "Contatos - "}
 
-    return render(
-        request,
-        'contact/index.html',
-        context
-    )
+    return render(request, "contact/index.html", context)
 
 
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
-    single_contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
-    )
-    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    site_title = f"{single_contact.first_name} {single_contact.last_name} - "
 
-    context = {
-        'contact': single_contact,
-        'site_title': site_title
-    }
+    context = {"contact": single_contact, "site_title": site_title}
 
-    return render(
-        request,
-        'contact/contact.html',
-        context
-    )
+    return render(request, "contact/contact.html", context)
+
+
+def search(request):
+    search_value = request.GET.get("q", "").strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) | 
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value) 
+        ).order_by("-id")
+    print(contacts.query)
+    print("resultados encontrados: ", len(contacts))
+
+    context = {"contacts": contacts, "site_title": "Search - "}
+    print(search_value)
+    return render(request, "contact/index.html", context)
