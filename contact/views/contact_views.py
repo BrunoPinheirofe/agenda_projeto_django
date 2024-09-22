2a34b13d3beb70b2b7728dad8d3a735c9dd0547f
Bruno Pinheiro <bruno2013pinheiro@gmail.com>
2024-09-22 16:03:35 -0300

    adicionando paginacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from contact.models import Contact
 
 
-
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by("-id")[10:20]
+    
+    
+    contacts = Contact.objects.filter(show=True).order_by("-id")
+    paginator = Paginator(contacts, 10) # 10 contatos por pagina
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
-    context = {"contacts": contacts, "site_title": "Contatos - "}
+    context = {"page_obj": page_obj, "site_title": "Contatos - "}
 
     return render(request, "contact/index.html", context)
 
@@ -25,18 +30,22 @@ def contact(request, contact_id):
 
 def search(request):
     search_value = request.GET.get("q", "").strip()
-    
-    if search_value == '':
-        return redirect('contact:index')
-    contacts = Contact.objects.filter(show=True).filter(
-        Q(first_name__icontains=search_value) | 
-        Q(last_name__icontains=search_value) |
-        Q(phone__icontains=search_value) |
-        Q(email__icontains=search_value) 
-        ).order_by("-id")
-    print(contacts.query)
-    print("resultados encontrados: ", len(contacts))
 
-    context = {"contacts": contacts, "site_title": "Search - "}
-    print(search_value)
+    if search_value == "":
+        return redirect("contact:index")
+    contacts = (
+        Contact.objects.filter(show=True)
+        .filter(
+            Q(first_name__icontains=search_value)
+            | Q(last_name__icontains=search_value)
+            | Q(phone__icontains=search_value)
+            | Q(email__icontains=search_value)
+        )
+        .order_by("-id")
+    )
+    paginator = Paginator(contacts, 10) # 10 contatos por pagina
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj, "site_title": "Contatos - "}
     return render(request, "contact/index.html", context)
